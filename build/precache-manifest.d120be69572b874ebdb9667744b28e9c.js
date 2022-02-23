530cdd842e79de8fc76d766294c05ff9b8d87536
aitq <aitu555@gmail.com>
2022-02-23 23:29:42 +0500

    new

---
+++ build/precache-manifest.d120be69572b874ebdb9667744b28e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ccea53e82251a57a6001140bced54d",
+    "url": "/atiqurrehman123/atiq-portfolio-work/index.html"
+  },
+  {
+    "revision": "fe15c632b23ed4abc6fb",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "3d5b3562b1d0ac32f672",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/css/main.d715ac16.chunk.css"
+  },
+  {
+    "revision": "fe15c632b23ed4abc6fb",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/js/2.9b3c987b.chunk.js"
+  },
+  {
+    "revision": "7c2bd0664b0714c43d4fa05f933fc9a3",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/js/2.9b3c987b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5b3562b1d0ac32f672",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/js/main.55d12356.chunk.js"
+  },
+  {
+    "revision": "6f1da4989e2ce583bf3e",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/js/runtime-main.8768b675.js"
+  },
+  {
+    "revision": "c793c56e7cdb4c2015d1b9c0ac2d4791",
+    "url": "/atiqurrehman123/atiq-portfolio-work/static/media/profilepic.c793c56e.jpeg"
+  }
+]);
